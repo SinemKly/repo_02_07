c45ed98de442c04625114ce047201f48d88483fc
sinebonisimo <sinebonisimo@sinem-pc>
2013-07-02 10:48:28 +0300

    kadir

--- src/g/G.java
+++ src/g/G.java
@@ -16,7 +16,7 @@ public class G {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("abc");
-        
+        System.err.println("abc");
+        System.err.println("abc");
     }
 }
